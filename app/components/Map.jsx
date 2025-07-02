cfca0441ffd3dd6b5c6fa9fa136ba362b0a4a599
cruz-jay <jay-cruz@outlook.com>
2025-07-02 12:08:54 -0500

    project complete

--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import useCitiesStore from "../store/useCitiesStore";
-const ACCESS_TOKEN = import.meta.env.ACCESS_TOKEN;
+const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
 
 const Map = () => {
   const currentCity = useCitiesStore((state) => state.currentCity);
